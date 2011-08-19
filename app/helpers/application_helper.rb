2c1cb103b7ecea05f8f324688cc279a39a85f71e
Joel Mahoney <joelmahoney@gmail.com>
2011-08-19 01:46:05 -0700

    tweak the walk zone and assignment zone descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def address_helper
+    if @geocoded_address.present?
+      "#{@geocoded_address.street_address}, #{@geocoded_address.city}, #{@geocoded_address.state}"
+    elsif params[:address].present?
+      params[:address]
+    end
+  end
 end
