0afe22370e5b3fac5ee3ca08b88d2dc7734a7b44
Joel Mahoney <joelmahoney@gmail.com>
2011-08-19 01:56:23 -0700

    dont show the geocoded_address if success is false

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def address_helper
-    if @geocoded_address.present?
+    if @geocoded_address.present? && @geocoded_address.success == true
       "#{@geocoded_address.street_address}, #{@geocoded_address.city}, #{@geocoded_address.state}"
     elsif params[:address].present?
       params[:address]
