3aab6a3e51f2dba40167abebc47a19470c5c842f
Joel Mahoney <joelmahoney@gmail.com>
2011-08-31 15:49:59 -0700

    add a geospatial parcel column to the db

--- db/migrate/20110830180243_add_parcel_coordinates_to_schools.rb
+++ db/migrate/20110830180243_add_parcel_coordinates_to_schools.rb
@@ -1,6 +1,6 @@
 class AddParcelCoordinatesToSchools < ActiveRecord::Migration
   def change
-    add_column :schools, :parcel, :polygon
+    add_column :schools, :parcel, :multi_polygon, :srid => 4326
     add_index :schools, :parcel, :spatial => true
   end
 end
