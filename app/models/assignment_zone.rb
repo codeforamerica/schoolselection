abb4bf981fe80c9f19ab29d988b06e3da2c04b06
Joel Mahoney <joelmahoney@gmail.com>
2011-09-01 16:55:10 -0700

    replace point in polygon finder methods with find_by_location

--- app/models/assignment_zone.rb
+++ app/models/assignment_zone.rb
@@ -7,16 +7,8 @@ class AssignmentZone < ActiveRecord::Base
   
   ####### CLASS METHODS #######
   
-  def self.find_all_with_location_and_grade_level(location, grade_level)
-    if grade_level.name == 'High'
-      self.all
-    else
-      self.find_all_with_point(location.lat, location.lng)
-    end
-  end
-    
-  def self.find_all_with_point(lat, lng)
-    self.all.select {|x| x.includes_point?(lat, lng)}
+  def self.find_by_location(location)
+    self.where("ST_Within(ST_Transform(ST_GeomFromText('POINT(#{location.lng} #{location.lat})', 4326), 2249), shape)")
   end
   
   ####### INSTANCE METHODS #######
@@ -27,28 +19,5 @@ class AssignmentZone < ActiveRecord::Base
   
   def schools_by_grade_level(grade_level)
     School.find_all_with_school_level(grade_level).select {|x| self.includes_point?(x.lat.to_f, x.lng.to_f) }
-  end
-  
-  def includes_point?(lat, lng)
-    polygon = self.coordinates
-    if polygon.blank?
-      return false
-    else
-      intersects = 0
-      p1 = polygon[0]
-      for i in 1..(polygon.size-1)
-        p2 = polygon[i % polygon.size]
-        if ((lat > [p1[0],p2[0]].min) &&
-          (lat <= [p1[0],p2[0]].max) &&
-          (lng <= [p1[1],p2[1]].max) &&
-          (p1[0] != p2[0]))
-          xinters = (lat-p1[0])*(p2[1]-p1[1])/(p2[0]-p1[0])
-          p1[1]
-          intersects += 1 if (p1[1] == p2[1] || lng <= xinters)
-        end
-        p1 = p2
-      end
-      (intersects % 2 == 1)
-    end
   end
 end
