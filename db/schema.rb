46c704be5d425b3263b0c28224a2b1ea5096b247
Joel Mahoney <joelmahoney@gmail.com>
2011-09-09 11:33:31 -0700

    update gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110901230748) do
+ActiveRecord::Schema.define(:version => 20110830005045) do
 
   create_table "assignment_zones", :force => true do |t|
-    t.string        "name"
-    t.datetime      "created_at"
-    t.datetime      "updated_at"
-    t.multi_polygon "shape",      :limit => nil, :srid => 2249
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "coordinates"
   end
-
-  add_index "assignment_zones", ["shape"], :name => "index_assignment_zones_on_shape", :spatial => true
 
   create_table "cities", :force => true do |t|
     t.string   "name"
@@ -36,6 +34,16 @@ ActiveRecord::Schema.define(:version => 20110901230748) do
     t.float    "lng"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "geometry_columns", :id => false, :force => true do |t|
+    t.string  "f_table_catalog",   :limit => 256, :null => false
+    t.string  "f_table_schema",    :limit => 256, :null => false
+    t.string  "f_table_name",      :limit => 256, :null => false
+    t.string  "f_geometry_column", :limit => 256, :null => false
+    t.integer "coord_dimension",                  :null => false
+    t.integer "srid",                             :null => false
+    t.string  "type",              :limit => 30,  :null => false
   end
 
   create_table "grade_levels", :force => true do |t|
@@ -92,52 +100,58 @@ ActiveRecord::Schema.define(:version => 20110901230748) do
   end
 
   create_table "schools", :force => true do |t|
-    t.string        "name"
-    t.text          "description"
-    t.text          "features"
-    t.string        "address"
-    t.integer       "city_id"
-    t.integer       "state_id"
-    t.string        "zipcode"
-    t.string        "phone"
-    t.string        "fax"
-    t.string        "website"
-    t.integer       "assignment_zone_id"
-    t.integer       "mail_cluster_id"
-    t.integer       "school_group_id"
-    t.string        "grades"
-    t.string        "hours"
-    t.string        "early_dismissal_time"
-    t.string        "breakfast"
-    t.string        "lunch"
-    t.string        "dinner"
-    t.integer       "principal_id"
-    t.datetime      "created_at"
-    t.datetime      "updated_at"
-    t.float         "lat"
-    t.float         "lng"
-    t.integer       "bpsid"
-    t.integer       "org_code"
-    t.integer       "teachers_count"
-    t.integer       "core_areas_teachers_count"
-    t.float         "licensed_teachers_percentage"
-    t.float         "qualified_teachers_percentage"
-    t.float         "qualified_classes_percentage"
-    t.float         "staff_to_student_ratio"
-    t.string        "school_level_name"
-    t.string        "school_type_name"
-    t.multi_polygon "parcel",                        :limit => nil, :srid => 2249
+    t.string   "name"
+    t.text     "description"
+    t.text     "features"
+    t.string   "address"
+    t.integer  "city_id"
+    t.integer  "state_id"
+    t.string   "zipcode"
+    t.string   "phone"
+    t.string   "fax"
+    t.string   "website"
+    t.integer  "assignment_zone_id"
+    t.integer  "mail_cluster_id"
+    t.integer  "school_group_id"
+    t.string   "grades"
+    t.string   "hours"
+    t.string   "early_dismissal_time"
+    t.string   "breakfast"
+    t.string   "lunch"
+    t.string   "dinner"
+    t.integer  "principal_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.float    "lat"
+    t.float    "lng"
+    t.integer  "bpsid"
+    t.integer  "org_code"
+    t.integer  "teachers_count"
+    t.integer  "core_areas_teachers_count"
+    t.float    "licensed_teachers_percentage"
+    t.float    "qualified_teachers_percentage"
+    t.float    "qualified_classes_percentage"
+    t.float    "staff_to_student_ratio"
+    t.string   "school_level_name"
+    t.string   "school_type_name"
   end
 
   add_index "schools", ["assignment_zone_id"], :name => "index_schools_on_assignment_zone_id"
   add_index "schools", ["mail_cluster_id"], :name => "index_schools_on_mail_cluster_id"
-  add_index "schools", ["parcel"], :name => "index_schools_on_parcel", :spatial => true
   add_index "schools", ["principal_id"], :name => "index_schools_on_principal_id"
   add_index "schools", ["school_group_id"], :name => "index_schools_on_school_group_id"
 
   create_table "schools_walk_zones", :id => false, :force => true do |t|
     t.integer "walk_zone_id"
     t.integer "school_id"
+  end
+
+  create_table "spatial_ref_sys", :id => false, :force => true do |t|
+    t.integer "srid",                      :null => false
+    t.string  "auth_name", :limit => 256
+    t.integer "auth_srid"
+    t.string  "srtext",    :limit => 2048
+    t.string  "proj4text", :limit => 2048
   end
 
   create_table "states", :force => true do |t|
