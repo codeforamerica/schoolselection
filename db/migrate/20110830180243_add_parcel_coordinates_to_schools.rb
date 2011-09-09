3e441ddfeb657288b5ce00b0f04853891d9c1f7b
Joel Mahoney <joelmahoney@gmail.com>
2011-09-09 11:33:24 -0700

    remove spatial_adapter migrations

--- db/migrate/20110830180243_add_parcel_coordinates_to_schools.rb
+++
@@ -1,6 +0,0 @@
-class AddParcelCoordinatesToSchools < ActiveRecord::Migration
-  def change
-    add_column :schools, :parcel, :multi_polygon, :srid => 4326
-    add_index :schools, :parcel, :spatial => true
-  end
-end
