aeee3216212651cacdec8641ee0f0ffa53af8d06
Joel Mahoney <joelmahoney@gmail.com>
2011-09-20 15:32:31 -0700

    use a constant for miles_to_meters instead of hard-coding

--- app/models/grade_level.rb
+++ app/models/grade_level.rb
@@ -2,7 +2,7 @@ class GradeLevel < ActiveRecord::Base
   has_and_belongs_to_many :schools
 
   def walk_zone_radius_in_meters
-    self.walk_zone_radius * 1609.344
+    self.walk_zone_radius * MILES_TO_METERS
   end
 
 end
