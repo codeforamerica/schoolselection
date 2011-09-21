78f4ed934089bd6a06d4ad4e922a505780d5daa7
T S <YenTheFirst@gmail.com>
2011-09-21 12:00:35 -0700

    add 'short string' formatting to collections of grade levels

--- app/models/grade_level.rb
+++ app/models/grade_level.rb
@@ -5,4 +5,14 @@ class GradeLevel < ActiveRecord::Base
     self.walk_zone_radius * METERS_PER_MILE
   end
 
+  def order_index
+    #currently, grades are K0,K1,K2,1,2,...
+    #this function will give us a canonical ordering, where K0=0,K2=2,1=3, ... 
+    number =~ /K(\d)/ ? $1.to_i : (number.to_i + 2)
+  end
+
+  def self.short_string #not sure if this should go in here or not
+    as_i = self.all.map(&:order_index).sort
+    self.all.slice_before {|x| !as_i.include?(x.order_index-1)}.map {|arr| "#{arr.first.number}..#{arr.last.number}"}*", "
+  end
 end
