6a4e071c2eb6f2babe460513682fa4f014b4a412
Joel Mahoney <joelmahoney@gmail.com>
2011-09-22 10:45:04 -0700

    switch back to Google geocoder to use bounds

--- lib/geography.rb
+++ lib/geography.rb
@@ -3,8 +3,8 @@ module Geography
   BOSTON = Geokit::Geocoders::GoogleGeocoder.geocode('Boston, MA')
   
   def geocode_address(address)
-    # Geokit::Geocoders::GoogleGeocoder.geocode(address, :bias => BOSTON.suggested_bounds)
-    Geokit::Geocoders::MultiGeocoder.geocode(address)
+    Geokit::Geocoders::GoogleGeocoder.geocode(address, :bias => BOSTON.suggested_bounds)
+    # Geokit::Geocoders::MultiGeocoder.geocode(address)
   end
 
 end
