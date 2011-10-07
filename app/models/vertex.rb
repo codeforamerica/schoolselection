f9761c780864c66554209a2bfbabce44f085e41e
T S <YenTheFirst@gmail.com>
2011-10-06 18:51:48 -0700

    rename Vertex#find_nearest to #nearest_to, and implement

--- app/models/vertex.rb
+++ app/models/vertex.rb
@@ -2,7 +2,10 @@ class Vertex < ActiveRecord::Base
   set_table_name "vertices_tmp"
 
   class << self
-    def find_nearest(location,max_distance=100)
+    def nearest_to(location,max_distance=100)
+      p = "ST_GeogFromText('POINT(#{location.lng.to_f} #{location.lat.to_f})')"
+      where("ST_DWithin(the_geom,#{p},?)",100)
+        .order("ST_Distance(the_geom,#{p})")
     end
   end
 
