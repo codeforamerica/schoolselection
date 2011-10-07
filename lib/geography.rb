d35c887d9a59ce07bc725dfe4ecc5b1c43ac6ad1
T S <YenTheFirst@gmail.com>
2011-10-07 16:31:52 -0700

    Geography::walkshed_for_point takes a vertex object, not an id

--- lib/geography.rb
+++ lib/geography.rb
@@ -6,8 +6,8 @@ module Geography
     # Geokit::Geocoders::MultiGeocoder.geocode(address)
   end
 
-  def walkshed_for_point(vertex_id,radius_in_km)
-    result = ActiveRecord::Base.connection.execute("select the_geom from points_as_polygon($$select vertex_id as id, ST_X(geometry(the_geom)) as x, ST_Y(geometry(the_geom)) as y from driving_distance(''SELECT gid as id, source, target, length as cost from ways where class_id >= 104'',#{vertex_id.to_i},#{radius_in_km.to_f}, false,false) join vertices_tmp on vertices_tmp.id = vertex_id$$);")
+  def walkshed_for_point(vertex,radius_in_km)
+    result = ActiveRecord::Base.connection.execute("select the_geom from points_as_polygon($$select vertex_id as id, ST_X(geometry(the_geom)) as x, ST_Y(geometry(the_geom)) as y from driving_distance(''SELECT gid as id, source, target, length as cost from ways where class_id >= 104'',#{vertex.id.to_i},#{radius_in_km.to_f}, false,false) join vertices_tmp on vertices_tmp.id = vertex_id$$);")
     result[0]["the_geom"]
   end
 
