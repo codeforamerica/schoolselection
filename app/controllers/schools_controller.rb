0dc7f68dfc85051b3338fdb68fdc7498847e1590
Joel Mahoney <joelmahoney@gmail.com>
2011-10-10 16:30:33 -0700

    work on school lightbox view

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -6,28 +6,10 @@ class SchoolsController < ApplicationController
     zipcode, session[:zipcode] = params[:zipcode], params[:zipcode]
     grade_level, session[:grade_level] = params[:grade_level], params[:grade_level]
     session[:favorites].present? ? @favorite_schools = session[:favorites].map {|x| School.find(x)} : @favorite_schools = []
-    @grade_levels = GradeLevel.all
     address.present? ? @geocoded_address = geocode_address("#{address}, #{zipcode}") : @geocoded_address = geocode_address('26 Court Street, Boston, MA 02109')
     
     if address.present? && grade_level.present? && @geocoded_address.success == true && AssignmentZone.find_by_location(@geocoded_address).present?
-      @grade_level = GradeLevel.find_by_number(session[:grade_level])
-      @assignment_zone = AssignmentZone.find_by_location(@geocoded_address).first
-
-      @walk_zone_schools = @grade_level.schools.find_all_within_radius(@geocoded_address, @grade_level.walk_zone_radius_in_meters).with_distance(@geocoded_address).order('distance ASC')
-      @assignment_zone_schools = @grade_level.schools.where(:assignment_zone_id => @assignment_zone).with_distance(@geocoded_address).order('distance ASC') - @walk_zone_schools
-      @citywide_schools = @grade_level.schools.where(:assignment_zone_id => AssignmentZone.citywide).with_distance(@geocoded_address).order('distance ASC') - @walk_zone_schools
-      @all_schools = (@walk_zone_schools + @assignment_zone_schools + @citywide_schools)
-      [ [@walk_zone_schools,"Walk Zone",1], [@assignment_zone_schools,"Assignment Zone",2], [@citywide_schools,"Citywide",3] ].each do |schools,type,index|
-        schools.each do |s|
-          s.eligibility = type
-          s.eligibility_index = index
-        end
-      end
-      if params[:sibling_school] && (sib_school = @all_schools.find {|s| s.id == params[:sibling_school].to_i})
-        #raise "in here"
-        sib_school.eligibility = "Sibling School / "+sib_school.eligibility
-        sib_school.eligibility_index = 0
-      end
+      shared_variables
     else
       @all_schools = School.all
     end
@@ -38,8 +20,9 @@ class SchoolsController < ApplicationController
   end
 
   def show
-    @favorite_schools = session[:favorites]
-    @school = School.find(params[:id])
+    shared_variables
+    session[:favorites].present? ? @favorite_schools = session[:favorites].map {|x| School.find(x)} : @favorite_schools = []
+    @school = @grade_level.schools.where(:id => params[:id]).with_distance(@geocoded_address).first
 
     respond_to do |format|
       format.html # show.html.erb
@@ -75,6 +58,31 @@ class SchoolsController < ApplicationController
     session[:favorites].present? ? @favorite_schools = session[:favorites].map {|x| School.find(x)} : @favorite_schools = []
     respond_to do |format|
       format.js
+    end
+  end
+  
+  private
+  
+  def shared_variables
+    @grade_levels = GradeLevel.all
+    @grade_level = GradeLevel.find_by_number(session[:grade_level])
+    @geocoded_address = geocode_address("#{session[:address]}, #{session[:zipcode]}")
+    @assignment_zone = AssignmentZone.find_by_location(@geocoded_address).first
+
+    @walk_zone_schools = @grade_level.schools.find_all_within_radius(@geocoded_address, @grade_level.walk_zone_radius_in_meters).with_distance(@geocoded_address).order('distance ASC')
+    @assignment_zone_schools = @grade_level.schools.where(:assignment_zone_id => @assignment_zone).with_distance(@geocoded_address).order('distance ASC') - @walk_zone_schools
+    @citywide_schools = @grade_level.schools.where(:assignment_zone_id => AssignmentZone.citywide).with_distance(@geocoded_address).order('distance ASC') - @walk_zone_schools
+    @all_schools = (@walk_zone_schools + @assignment_zone_schools + @citywide_schools)
+    [ [@walk_zone_schools,"Walk Zone",1], [@assignment_zone_schools,"Assignment Zone",2], [@citywide_schools,"Citywide",3] ].each do |schools,type,index|
+      schools.each do |s|
+        s.eligibility = type
+        s.eligibility_index = index
+      end
+    end
+    if params[:sibling_school] && (sib_school = @all_schools.find {|s| s.id == params[:sibling_school].to_i})
+      #raise "in here"
+      sib_school.eligibility = "Sibling School / "+sib_school.eligibility
+      sib_school.eligibility_index = 0
     end
   end
 end
