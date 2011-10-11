c3a6bc2b420044032e2c6698f2b00a244abe7fff
Joel Mahoney <joelmahoney@gmail.com>
2011-10-10 23:36:04 -0700

    add demand to admissions tab

--- app/models/school_grade.rb
+++ app/models/school_grade.rb
@@ -1,4 +1,21 @@
 class SchoolGrade < ActiveRecord::Base
   belongs_to :school
   belongs_to :grade_level
+  
+  def total_choices
+    if first_choice.present?
+      total = 0
+      total += first_choice
+      total += second_choice
+      total += third_choice
+      total += fourth_higher_choice
+      total
+    end
+  end
+  
+  def demand
+    if total_choices.present?
+      (total_choices.to_f / open_seats) * 100
+    end
+  end
 end
