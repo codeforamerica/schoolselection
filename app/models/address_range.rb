b32d176f2c855b51385190ebb008cc92a111ad28
T S <YenTheFirst@gmail.com>
2011-11-02 15:05:50 -0700

    create helper method for address range finding

--- app/models/address_range.rb
+++ app/models/address_range.rb
@@ -1,3 +1,9 @@
 class AddressRange < ActiveRecord::Base
   belongs_to :geocode
+
+  class << self
+    def find_by_address(num,street_name,zipcode)
+      where(:street=>street_name.upcase.strip, :zipcode=>zipcode, :is_even=>num.even?).where("num_start <= ? AND num_end >= ?",num,num)
+    end
+  end
 end
