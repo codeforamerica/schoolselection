4683ef9ed59e865205eca37e0644f6c875dcbbe7
Joel Mahoney <joelmahoney@gmail.com>
2011-11-09 00:17:20 -0800

    fix favorite_schools sort order when accessing from session[:favorites]

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -44,12 +44,7 @@ class SchoolsController < ApplicationController
   end
   
   def compare
-    @grade_level = GradeLevel.find_by_number(session[:grade_level])
-    @geocoded_address ||= geocode_address("#{session[:address]}, #{session[:zipcode]}")
-    @favorite_schools = @grade_level.schools.where("schools.id IN (?)", session[:favorites])
-      .select("schools.*")
-      .with_distance(@geocoded_address)
-      .includes(:grade_level_schools, :city)
+    @favorite_schools = session[:favorites].map {|x| School.find(x)}
   end
   
   ####### AJAX #######
